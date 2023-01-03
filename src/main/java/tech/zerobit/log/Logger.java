0b8134288564bc9f9164bb4bff2833e1b483f886
simon44556 <simon44556@gmail.com>
2023-01-03 14:11:25 +0100

    - Add spread operator option

--- src/main/java/tech/zerobit/log/Logger.java
+++ src/main/java/tech/zerobit/log/Logger.java
@@ -14,4 +14,16 @@ public class Logger extends ALogger {
     WarnLogger.log(message);
   }
 
+  public static void log(String... message) {
+    InfoLogger.log(String.join(" ", message));
+  }
+
+  public static void error(String... message) {
+    ErrorLogger.log(String.join(" ", message));
+  }
+
+  public static void warn(String... message) {
+    WarnLogger.log(String.join(" ", message));
+  }
+
 }
