e5eee1184b42b498a60c41dfc95724a1772ea5b2
Zero <zeromaniac.mc@gmail.com>
2023-01-03 14:03:10 +0100

    i hate github

--- src/main/java/tech/zerobit/TestCalls.java
+++ src/main/java/tech/zerobit/TestCalls.java
@@ -1,6 +1,7 @@
 package tech.zerobit;
 
 import tech.zerobit.formatter.Beautifier;
+import tech.zerobit.time.TimeConverter;
 import tech.zerobit.validate.UUIDValidator;
 
 public class TestCalls {
@@ -9,6 +10,7 @@ public class TestCalls {
         System.out.println(Beautifier.replaceUnderscoresWithSpaces("world_the_end"));
         System.out.println(Beautifier.capitalizeAllWords(""));
         System.out.println(UUIDValidator.isValidUUID("4db79777-69d4-455d-a9ba-c1c96d225df5"));
+        System.out.println(TimeConverter.convertSingleUnitByName(5,"years", "months"));
     }
 
 }
