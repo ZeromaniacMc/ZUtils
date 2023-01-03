778bfc6db70548965e6d5df1e75ca3842d37f77a
Zero <zeromaniac.mc@gmail.com>
2023-01-03 15:51:52 +0100

    translation config

--- src/main/java/tech/zerobit/time/Translation.java
+++ src/main/java/tech/zerobit/time/Translation.java
@@ -1,22 +1,22 @@
 package tech.zerobit.time;
 
 public enum Translation {
-    MILLISECOND_SINGULAR("Translations.Millisecond.Singular", "millisecond"),
-    MILLISECOND_PLURAL("Translations.Millisecond.Plural", "milliseconds"),
-    SECOND_SINGULAR("Translations.Second.Singular", "second"),
-    SECOND_PLURAL("Translations.Second.Plural", "seconds"),
-    MINUTE_SINGULAR("Translations.Minute.Singular", "minute"),
-    MINUTE_PLURAL("Translations.Minute.Plural", "minutes"),
-    HOUR_SINGULAR("Translations.Hour.Singular", "hour"),
-    HOUR_PLURAL("Translations.Hour.Plural", "hours"),
-    DAY_SINGULAR("Translations.Day.Singular", "day"),
-    DAY_PLURAL("Translations.Day.Plural", "days"),
-    WEEK_SINGULAR("Translations.Week.Singular", "week"),
-    WEEK_PLURAL("Translations.Week.Plural", "weeks"),
-    MONTH_SINGULAR("Translations.Month.Singular", "month"),
-    MONTH_PLURAL("Translations.Month.Plural", "months"),
-    YEAR_SINGULAR("Translations.Year.Singular", "year"),
-    YEAR_PLURAL("Translations.Year.Plural","years");
+    MILLISECOND_SINGULAR("TimeUnit.Millisecond.Singular", "millisecond"),
+    MILLISECOND_PLURAL("TimeUnit.Millisecond.Plural", "milliseconds"),
+    SECOND_SINGULAR("TimeUnit.Second.Singular", "second"),
+    SECOND_PLURAL("TimeUnit.Second.Plural", "seconds"),
+    MINUTE_SINGULAR("TimeUnit.Minute.Singular", "minute"),
+    MINUTE_PLURAL("TimeUnit.Minute.Plural", "minutes"),
+    HOUR_SINGULAR("TimeUnit.Hour.Singular", "hour"),
+    HOUR_PLURAL("TimeUnit.Hour.Plural", "hours"),
+    DAY_SINGULAR("TimeUnit.Day.Singular", "day"),
+    DAY_PLURAL("TimeUnit.Day.Plural", "days"),
+    WEEK_SINGULAR("TimeUnit.Week.Singular", "week"),
+    WEEK_PLURAL("TimeUnit.Week.Plural", "weeks"),
+    MONTH_SINGULAR("TimeUnit.Month.Singular", "month"),
+    MONTH_PLURAL("TimeUnit.Month.Plural", "months"),
+    YEAR_SINGULAR("TimeUnit.Year.Singular", "year"),
+    YEAR_PLURAL("TimeUnit.Year.Plural","years");
 
 
     public String path;
